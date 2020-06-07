b62d53a2e3e6d2b8ee5acc6430b245347e595285
Matyáš  Řezníček <matyasreznicek@Matyass-MacBook-Pro.local>
2020-06-07 18:03:51 +0200

    set to heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT
+
+
 const publicFilePath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
 const partialPath = path.join(__dirname, '../templates/partials')
@@ -79,6 +82,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up and running')
+app.listen(port, () => {
+    console.log(`server is up and running on ${port}`)
 })
