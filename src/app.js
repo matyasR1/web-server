80685790de9f9ea074d58423742bde8dea21ceb8
Matyáš  Řezníček <matyasreznicek@Matyass-MacBook-Pro.local>
2020-06-07 18:36:59 +0200

    edited wind description in forecast

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 
 const publicFilePath = path.join(__dirname, '../public')
